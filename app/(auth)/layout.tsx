90e47bce02969b92a5bbdc9e9e379275ab9fc135
samabdullaev <abdullaevsamandar2004@gmail.com>
2025-04-04 10:44:02 +0100

    unnecessary files removed

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,18 +1,18 @@
-import { ReactNode } from "react";
+import { ReactNode } from 'react';
 
 const layout = ({ children }: { children: ReactNode }) => {
-  return (
-    <html>
-      <head>
-        <meta name="author" content="Softnio" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="shortcut icon" href="images/favicon.png" />
-        <title>Googlers - Insights from Uzbek Engineers at Google</title>
-        <link rel="stylesheet" href="assets/css/style6572.css?v1.5.0" />
-      </head>
-      <body>{children}</body>
-    </html>
-  );
+	return (
+		<html>
+			<head>
+				<meta name='author' content='Softnio' />
+				<meta name='viewport' content='width=device-width, initial-scale=1.0' />
+				<link rel='shortcut icon' href='images/favicon.png' />
+				<title>Smart Resume Matcher</title>
+				<link rel='stylesheet' href='assets/css/style6572.css?v1.5.0' />
+			</head>
+			<body>{children}</body>
+		</html>
+	);
 };
 
 export default layout;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,27 @@
-import { ThemeProvider } from "next-themes";
-import { ReactNode } from "react";
+import { ThemeProvider } from 'next-themes';
+import { ReactNode } from 'react';
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
-  return (
-    <html>
-      <head>
-        <meta name="author" content="Softnio" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="shortcut icon" href="images/favicon.png" />
-        <title>Googlers - Insights from Uzbek Engineers at Google</title>
-        <link rel="stylesheet" href="assets/css/style6572.css?v1.5.0" />
-      </head>
-      <body>
-        {/* <ThemeProvider
+	return (
+		<html>
+			<head>
+				<meta name='author' content='Softnio' />
+				<meta name='viewport' content='width=device-width, initial-scale=1.0' />
+				<link rel='shortcut icon' href='images/favicon.png' />
+				<title>Smart Resume Matcher</title>
+				<link rel='stylesheet' href='assets/css/style6572.css?v1.5.0' />
+			</head>
+			<body>
+				{/* <ThemeProvider
           attribute="class"
           defaultTheme="light"
           enableSystem={false}
         > */}
-        {children}
-        {/* </ThemeProvider> */}
-      </body>
-    </html>
-  );
+				{children}
+				{/* </ThemeProvider> */}
+			</body>
+		</html>
+	);
 };
 
 export default RootLayout;
